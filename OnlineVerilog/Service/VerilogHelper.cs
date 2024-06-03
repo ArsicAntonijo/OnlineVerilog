b5508e3479f00f28a90cc90919954ea74eca9c5e
Antonijo Arsic <antonijo.arsic@hpe.com>
2024-06-03 15:45:26 +0200

    unique directory per user and modified paths for linux

--- OnlineVerilog/Service/VerilogHelper.cs
+++ OnlineVerilog/Service/VerilogHelper.cs
@@ -4,17 +4,25 @@ namespace OnlineVerilog.Service
 {
     public class VerilogHelper
     {
-        private readonly string WorkingDirectory = "C:\\Users\\arsica\\source\\repos\\OnlineVerilog\\OnlineVerilog\\Env\\";
+        private readonly static string WorkingDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Env");
+        private string TempDirectory = string.Empty;
         private readonly string ExeName = "tmp";
+        static VerilogHelper() 
+        {
+            if (!Directory.Exists(WorkingDirectory)) { Directory.CreateDirectory(WorkingDirectory); }           
+        }
         public string ExecuteTheProcess(string fileName, string fileContent)
         {
-            File.WriteAllText(WorkingDirectory + fileName, fileContent);
+            TempDirectory = Path.Combine(WorkingDirectory, DateTime.Now.ToString("yyMMddHHmmfffffff"));
+            if (!Directory.Exists(TempDirectory)) { Directory.CreateDirectory(TempDirectory); }
+            File.WriteAllText(Path.Combine(TempDirectory, fileName), fileContent);
             string compileOutput = Compile(fileName);
             if (string.IsNullOrEmpty(compileOutput))
             {
                 string runOutput = Run();
                 return runOutput;
             }
+            if (Directory.Exists(TempDirectory)) { Directory.Delete(TempDirectory, true); }
             return compileOutput;
         }
         private string Compile(string fileName)
@@ -22,7 +30,7 @@ namespace OnlineVerilog.Service
             string outp = string.Empty;
             Process p = new();
             p.StartInfo.FileName = "iverilog";
-            p.StartInfo.WorkingDirectory = WorkingDirectory;
+            p.StartInfo.WorkingDirectory = TempDirectory;
             p.StartInfo.Arguments = $" -o {ExeName} {fileName}";
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
@@ -40,7 +48,7 @@ namespace OnlineVerilog.Service
             string outp = string.Empty;
             Process p = new();
             p.StartInfo.FileName = "vvp";
-            p.StartInfo.WorkingDirectory = WorkingDirectory;
+            p.StartInfo.WorkingDirectory = TempDirectory;
             p.StartInfo.Arguments = ExeName;
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
