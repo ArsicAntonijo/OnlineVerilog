14210ecc39d9ac65de4503a0f9649de95abd6107
Antonijo Arsic <antonijo.arsic@hpe.com>
2024-06-09 18:18:44 +0200

    initial authorization logic added

---
+++ OnlineVerilog/Pages/Auth/LogIn.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OnlineVerilog.Context;
+using OnlineVerilog.Models;
+
+namespace OnlineVerilog.Pages.Auth
+{
+    public class LogInModel : PageModel
+    {
+        private readonly OnlineVerilog.Context.VeronContext _context;
+
+        public LogInModel(OnlineVerilog.Context.VeronContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            User? u = _context.Users.Where(x => x.Email == User.Email).FirstOrDefault();
+            if (u != null && u.Password.Equals(User.Password))
+            {
+                return RedirectToPage("../Index");
+            }
+            return Page();
+        }
+    }
+}
