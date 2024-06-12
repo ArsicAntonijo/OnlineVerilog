2c18c8b891147c9b0a92e1a8a41531876e96ba72
Antonijo Arsic <antonijo.arsic@hpe.com>
2024-06-12 12:58:04 +0200

    component with list of examples added

---
+++ OnlineVerilog/ViewComponents/ExamplesViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineVerilog.Context;
+
+namespace OnlineVerilog.ViewComponents
+{
+    public class ExamplesViewComponent : ViewComponent
+    {
+        private VeronContext _context;
+        public ExamplesViewComponent(VeronContext vr) { _context = vr; }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var examples = _context.Examples.ToList();
+            return View(examples);
+        }
+    }
+}
