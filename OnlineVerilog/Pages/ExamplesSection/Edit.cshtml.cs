322df0ade73493230b0119d7fe132e50bdeffbbe
Antonijo Arsic <antonijo.arsic@hpe.com>
2024-07-22 16:48:55 +0200

    added Edit and List pages

---
+++ OnlineVerilog/Pages/ExamplesSection/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineVerilog.Context;
+using OnlineVerilog.Models;
+
+namespace OnlineVerilog.Pages.ExamplesSection
+{
+    public class EditModel : PageModel
+    {
+        private readonly OnlineVerilog.Context.VeronContext _context;
+
+        public EditModel(OnlineVerilog.Context.VeronContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Example Example { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var example =  await _context.Examples.FirstOrDefaultAsync(m => m.Id == id);
+            if (example == null)
+            {
+                return NotFound();
+            }
+            Example = example;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Example).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExampleExists(Example.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./List");
+        }
+
+        private bool ExampleExists(int id)
+        {
+            return _context.Examples.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ OnlineVerilog/Pages/ExamplesSection/List.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OnlineVerilog.Context;
+using OnlineVerilog.Models;
+
+namespace OnlineVerilog.Pages.ExamplesSection
+{
+    public class ListModel : PageModel
+    {
+        private readonly OnlineVerilog.Context.VeronContext _context;
+
+        public ListModel(OnlineVerilog.Context.VeronContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Example> Example { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Example = await _context.Examples.ToListAsync();
+        }
+    }
+}
